af5cd04565b151a1f3726b67ed882ef25e4ca71e
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-10-30 12:09:33 +0100

    Finders

--- app/src/androidTest/java/com/example/testing_ui/components/KiritoComponentTest.kt
+++ app/src/androidTest/java/com/example/testing_ui/components/KiritoComponentTest.kt
@@ -1,6 +1,12 @@
 package com.example.testing_ui.components
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithContentDescription
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,6 +20,15 @@ class KiritoComponentTest {
         composeTestRule.setContent {
             KiritoComponent()
         }
+
+        //Finder
+        composeTestRule.onNodeWithText("kirito", ignoreCase = true)
+        composeTestRule.onNodeWithTag("component3")
+        composeTestRule.onNodeWithContentDescription("superImage")
+
+        composeTestRule.onAllNodesWithText(":") // it will search by letter or simbol
+        composeTestRule.onAllNodesWithTag("component1") // it will take all nodes that has the same ID
+        composeTestRule.onAllNodesWithContentDescription("VisualIcon")  // ti will search any type of view: (Decoration,button, etc..)
     }
-    
+
 }
--- app/src/main/java/com/example/testing_ui/components/KiritoComponent.kt
+++ app/src/main/java/com/example/testing_ui/components/KiritoComponent.kt
@@ -1,8 +1,23 @@
 package com.example.testing_ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 @Composable
 fun KiritoComponent(){
     //
+    Column (Modifier.fillMaxSize()){
+        Text(text = "KIRITO", Modifier.testTag("component1"))
+        Text(text = "PEDRO", Modifier.testTag("component2"))
+        Text(text = "KIRITO",Modifier.testTag("component3"))
+
+        Image(Icons.Default.Add, contentDescription = "superImage")
+    }
 }
