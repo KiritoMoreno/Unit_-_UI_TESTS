5147a27c4a30f806d7a01dc29f4bce3ff2ec32cd
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-10-30 14:25:51 +0100

    Actions

--- app/src/androidTest/java/com/example/testing_ui/components/KiritoComponentTest.kt
+++ app/src/androidTest/java/com/example/testing_ui/components/KiritoComponentTest.kt
@@ -1,12 +1,26 @@
 package com.example.testing_ui.components
 
+import androidx.compose.ui.test.doubleClick
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.longClick
 import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performImeAction
+import androidx.compose.ui.test.performScrollTo
+import androidx.compose.ui.test.performTextClearance
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.performTextReplacement
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipeDown
+import androidx.compose.ui.test.swipeLeft
+import androidx.compose.ui.test.swipeRight
+import androidx.compose.ui.test.swipeUp
 import org.junit.Rule
 import org.junit.Test
 
@@ -29,6 +43,26 @@ class KiritoComponentTest {
         composeTestRule.onAllNodesWithText(":") // it will search by letter or simbol
         composeTestRule.onAllNodesWithTag("component1") // it will take all nodes that has the same ID
         composeTestRule.onAllNodesWithContentDescription("VisualIcon")  // ti will search any type of view: (Decoration,button, etc..)
+
+        //Actions
+        composeTestRule.onNodeWithText("kirito", ignoreCase = true).performClick() // it can be clickable the text
+        composeTestRule.onAllNodesWithText("a").onFirst().performClick() //Take only one
+        composeTestRule.onNodeWithText("kirito").performTouchInput {
+            longClick()
+            doubleClick()
+            swipeDown()
+            swipeUp()
+            swipeLeft()
+            swipeRight()
+        }  // To select the click
+        composeTestRule.onNodeWithText("kirito").performScrollTo()  // Scroll inside the component
+        composeTestRule.onNodeWithText("kirito").performImeAction()  // The button of action (Enter)
+        composeTestRule.onNodeWithText("kirito").performTextClearance()  // Delete the Text of textField
+        composeTestRule.onNodeWithText("kirito").performTextInput("Hellllllooooo")  // it will add to the TextField
+        composeTestRule.onNodeWithText("kirito").performTextReplacement("Hello") // it will replace the text
+
+
+
     }
 
 }
--- app/src/main/java/com/example/testing_ui/components/KiritoComponent.kt
+++ app/src/main/java/com/example/testing_ui/components/KiritoComponent.kt
@@ -5,15 +5,30 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
 fun KiritoComponent(){
-    //
+
+    var name by rememberSaveable {
+        mutableStateOf("Kirito")
+    }
     Column (Modifier.fillMaxSize()){
+
+        TextField(value=name, onValueChange={name = it})
+        Text(text = "Your name $name")
         Text(text = "KIRITO", Modifier.testTag("component1"))
         Text(text = "PEDRO", Modifier.testTag("component2"))
         Text(text = "KIRITO",Modifier.testTag("component3"))
